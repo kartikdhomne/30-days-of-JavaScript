7800dec9823343624cbaa6a2a5ba1d1f3694afad
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-11-27 00:59:03 +0530

    Datatypes, Conversion, Comparison Operators

---
+++ Day-2.js
@@ -0,0 +1,129 @@
+// // Topic :- Datatypes
+
+// // *******************Primitive datatypes********************
+
+// //String
+// let name = "hitesh"
+
+// //Number
+// let age = 18
+
+// //Boolean
+// let isLoggedIn = false
+
+// //Undefined
+// let state;
+
+// //Null
+// let temp = null //standalone value
+
+// //BigInt
+// let num = 432534612432534112132351n //too big value
+
+// //Sysmbols
+// //unique identifier
+
+// // *******************Non-Primitive datatypes********************
+
+// //Object
+
+// let obj = new Object()
+
+// //Array
+
+// let arr = []
+
+// //function
+// let function () {
+
+// }
+
+// ----------------------------Conversion-----------------------------
+
+let score = "hitesh"
+
+//console.log(typeof score); // string
+//console.log(typeof(score)); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let isLoggedIn = "hitesh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// //  console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
+
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+// ------------------------------Comparison Operator---------------------------------------
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
