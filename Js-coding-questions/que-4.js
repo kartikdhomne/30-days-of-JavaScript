043645fd4b0a819d26fce4fa32fa9069b7b422ef
Kartik Dhomne <kartikdhomne1997@gmail.com>
2025-12-16 23:39:00 +0530

    This Keyword

---
+++ Js-coding-questions/que-4.js
@@ -0,0 +1,2 @@
+// Remove duplicate from sorted array
+
---
+++ Js-coding-questions/que-7.js
@@ -0,0 +1,16 @@
+// Palindrome Number
+
+// Example:-
+Input: x = 121
+Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+//Example 2:-
+Input: x = 10
+Output: false
+// Explanation: Reads 01 from right to left.Therefore it is not a palindrome.
+
+function isPalindrome (){
+    
+}
+
